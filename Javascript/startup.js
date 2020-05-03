21c18bd6d3d7e31b8ccc58d0622fe9acfe50c37c
kevinmattraka <kevinmattraka@gmail.com>
2020-05-02 21:34:01 -0600

    Support for currencies

--- Javascript/startup.js
+++ Javascript/startup.js
@@ -25,6 +25,18 @@ function loadprogress(){
         //shows the rest of the game
         y.style.display = "block";
         z.style.display = "block";
+        if (state.showsilver == true) {
+            document.getElementById("showsilver").style.display = "block";
+            document.getElementById("silver").innerHTML = state.silver;
+            if (state.showgold == true) {
+                document.getElementById("showgold").style.display = "block";
+                document.getElementById("gold").innerHTML = state.gold;
+                if (state.showplatinum == true) {
+                    document.getElementById("showplatinum").style.display = "block";
+                    document.getElementById("platinum").innerHTML = state.platinum;
+                }
+            }
+        }
     }
     document.getElementById("copper").innerHTML = state.copper;
     document.addEventListener("click", claimCharge);
